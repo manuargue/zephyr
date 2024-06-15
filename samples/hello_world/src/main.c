f13c7bd0e54427213d897c49b29c04ec2fade551
Manuel Argüelles <manuel.arguelles@nxp.com>
2024-06-15 09:29:48 +0700

    debug
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,17 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/linker/linker-defs.h>
+#include <zephyr/arch/arm/cortex_a_r/lib_helpers.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	printf("SCTLR.FI=%lx\n", read_sctlr() & BIT(21));
+
+	uint64_t tmp = read_sysreg64(0, 15);
+	printf("CPUACTLR=%llx (.OOODIVDIS=%lld)\n", tmp, (tmp >> 32) & BIT(13));
 
 	return 0;
 }
