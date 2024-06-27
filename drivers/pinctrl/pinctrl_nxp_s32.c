7a3dc99036e3ee7e50d022d0ce04df8781c725b6
Manuel Argüelles <marguelles.dev@gmail.com>
2024-06-27 09:47:15 +0700

    drivers: pinctrl: nxp: turn siul2 to native driver
    Signed-off-by: Manuel Argüelles <marguelles.dev@gmail.com>

--- drivers/pinctrl/pinctrl_nxp_s32.c
+++ drivers/pinctrl/pinctrl_nxp_s32.c
@@ -1,24 +1,94 @@
 /*
  * Copyright 2022 NXP
+ * Copyright 2024 Manuel Arguelles
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <zephyr/drivers/pinctrl.h>
 
-#include <Siul2_Port_Ip.h>
+// TODO: this fixup is already done in the pinmuxing script generator
+
+// #if defined(CONFIG_SOC_SERIES_S32K3)
+// #define SIUL2_0_IMCR_IDX_END 378
+// #elif defined(CONFIG_SOC_SERIES_S32ZE)
+// #define SIUL2_0_IMCR_IDX_END 89
+// #define SIUL2_1_IMCR_IDX_END 209
+// #define SIUL2_3_IMCR_IDX_END 23
+// #define SIUL2_4_IMCR_IDX_END 371
+// #define SIUL2_5_IMCR_IDX_END 474
+// #else
+// #error "platform not supported"
+// #endif /* CONFIG_SOC_SERIES_S32K3 */
+
+// static inline void siul2_get_imcr_reg(uint32_t *imcr_idx, uint8_t *siul2_idx)
+// {
+// #if defined(CONFIG_SOC_SERIES_S32K3)
+// 	if (*imcr_idx < SIUL2_IMCR_MAX_IDX) {
+// 		*siul2_idx = 0;
+// 	} else {
+// 		*siul2_idx = 1;
+// 		*imcr_idx = imcr_idx - SIUL2_IMCR_MAX_IDX;
+// 	}
+
+// #elif defined(CONFIG_SOC_SERIES_S32ZE)
+// 	if (*imcr_idx < SIUL2_IMCR_MAX_IDX) {
+// 		if (*imcr_idx <= SIUL2_0_IMCR_IDX_END) {
+// 			*siul2_idx = 0;
+// 		} else if (*imcr_idx <= SIUL2_1_IMCR_IDX_END) {
+// 			*siul2_idx = 1;
+// 		} else if (*imcr_idx <= SIUL2_4_IMCR_IDX_END) {
+// 			*siul2_idx = 4;
+// 		} else if (*imcr_idx <= SIUL2_5_IMCR_IDX_END) {
+// 			*siul2_idx = 5;
+// 		}
+// 	} else {
+// 		*siul2_idx = 3;
+// 		*imcr_idx = *imcr_idx - SIUL2_IMCR_MAX_IDX;
+// 	}
+
+// #else
+// #error "platform not supported"
+// #endif /* CONFIG_SOC_SERIES_S32K3 */
+// }
+
+/** Utility macro that expands to the SIUL2 base address if it exists */
+#define SIUL2_BASE_OR_ZERO(nodelabel)                                                              \
+	COND_CODE_1(DT_NODE_EXISTS(DT_NODELABEL(nodelabel)),                                       \
+		    (DT_REG_ADDR(DT_NODELABEL(nodelabel))), (0U))
+
+static mem_addr_t siul2_bases[] = {
+	SIUL2_BASE_OR_ZERO(siul2_0), SIUL2_BASE_OR_ZERO(siul2_1), SIUL2_BASE_OR_ZERO(siul2_2),
+	SIUL2_BASE_OR_ZERO(siul2_3), SIUL2_BASE_OR_ZERO(siul2_4), SIUL2_BASE_OR_ZERO(siul2_5),
+};
+
+static void pinctrl_configure_pin(const pinctrl_soc_pin_t *pin)
+{
+	mem_addr_t base;
+
+	__ASSERT_NO_MSG(pin->siul2_idx < ARRAY_SIZE(siul2_bases));
+	base = siul2_bases[pin->siul2_idx];
+	__ASSERT_NO_MSG(base != 0);
+
+	/* Multiplexed Signal Configuration */
+	__ASSERT_NO_MSG(pin->mscr_idx < SIUL2_MSCR_MAX_IDX);
+	sys_write32(pin->mscr_val, (base + SIUL2_MSCR(pin->mscr_idx)));
+
+	if (pin->mscr_val & SIUL2_MSCR_IBE) {
+		/* Input Multiplexed Signal Configuration */
+		__ASSERT_NO_MSG(pin->imcr_idx < SIUL2_IMCR_MAX_IDX);
+		sys_write32(pin->imcr_val, (base + SIUL2_IMCR(pin->imcr_idx)));
+	}
+}
 
 int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt, uintptr_t reg)
 {
-	/*
-	 * By invoking Siul2_Port_Ip_Init multiple times on each group of pins,
-	 * some functions like Siul2_Port_Ip_GetPinConfiguration and
-	 * Siul2_Port_Ip_RevertPinConfiguration cannot be used since the internal
-	 * state is not preserved between calls. Nevertheless, those functions
-	 * are not needed to implement Pinctrl driver, so it's safe to use it
-	 * until a public API exists to init each pin individually.
-	 */
-	Siul2_Port_Ip_Init(pin_cnt, pins);
+	ARG_UNUSED(reg);
+
+	for (uint8_t i = 0; i < pin_cnt; i++) {
+		pinctrl_configure_pin(pins++);
+	}
 
 	return 0;
 }
--- soc/nxp/s32/common/pinctrl_soc.h
+++ soc/nxp/s32/common/pinctrl_soc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2022-2023 NXP
+ * Copyright 2024 Manuel Arguelles
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,120 +10,91 @@
 
 #include <zephyr/devicetree.h>
 #include <zephyr/dt-bindings/pinctrl/nxp-s32-pinctrl.h>
-#include <zephyr/sys/util_macro.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/types.h>
-
-#include <Siul2_Port_Ip.h>
 
 /** @cond INTERNAL_HIDDEN */
 
-/** @brief Type for NXP S32 pin configuration. */
-typedef Siul2_Port_Ip_PinSettingsConfig pinctrl_soc_pin_t;
+/** @brief Type for NXP SIUL2 pin configuration. */
+typedef struct {
+	uint8_t siul2_idx;
+	uint32_t imcr_idx;
+	uint32_t imcr_val;
+	uint32_t mscr_idx;
+	uint32_t mscr_val;
+} pinctrl_soc_pin_t;
 
-/* Alias for compatibility with previous RTD versions */
-#if !defined(FEATURE_SIUL2_MAX_NUMBER_OF_INPUT) && defined(FEATURE_SIUL2_MAX_NUMBER_OF_INPUT_U8)
-#define FEATURE_SIUL2_MAX_NUMBER_OF_INPUT FEATURE_SIUL2_MAX_NUMBER_OF_INPUT_U8
-#endif
+#define SIUL2_MSCR_MAX_IDX 2048
+#define SIUL2_IMCR_MAX_IDX 512
 
-#if defined(SIUL2_PORT_IP_MULTIPLE_SIUL2_INSTANCES)
-#define NXP_S32_SIUL2_IDX(n)								\
-	n == 0 ? IP_SIUL2_0 : (n == 1 ? IP_SIUL2_1 : (					\
-	n == 3 ? IP_SIUL2_3 : (n == 4 ? IP_SIUL2_4 : (					\
-	n == 5 ? IP_SIUL2_5 : (NULL)))))
+// TODO: move to the soc folder and include it here
+// #include "pinctrl_soc_device.h"
+
+#if defined(CONFIG_SOC_SERIES_S32K3)
+#define SIUL2_MSCR(n)  (0x240 + 4 * (n))
+#define SIUL2_MSCR_OBE BIT(21)
+#define SIUL2_MSCR_IBE BIT(19)
+#define SIUL2_MSCR_INV BIT(17)
+#define SIUL2_MSCR_PKE BIT(16)
+#define SIUL2_MSCR_SRC BIT(14)
+#define SIUL2_MSCR_PUE BIT(13)
+#define SIUL2_MSCR_PUS BIT(11)
+#define SIUL2_MSCR_DSE BIT(8)
+#define SIUL2_MSCR_IFE BIT(6)
+#define SIUL2_MSCR_SMC BIT(5)
+#define SIUL2_MSCR_SSS GENMASK(3, 0)
+
+#define SIUL2_IMCR(n)  (0xA40 + 4 * (n))
+#define SIUL2_IMCR_SSS GENMASK(3, 0)
+
+#elif defined(CONFIG_SOC_SERIES_S32ZE)
+#define SIUL2_MSCR      0x240
+#define SIUL2_MSCR_OBE  BIT(21)
+#define SIUL2_MSCR_ODE  BIT(20)
+#define SIUL2_MSCR_IBE  BIT(19)
+#define SIUL2_MSCR_RXCB BIT(17)
+#define SIUL2_MSCR_SRE  GENMASK(16, 14)
+#define SIUL2_MSCR_PUE  BIT(13)
+#define SIUL2_MSCR_PUS  BIT(12)
+#define SIUL2_MSCR_CREF BIT(11)
+#define SIUL2_MSCR_RCVR BIT(10)
+#define SIUL2_MSCR_TRC  BIT(8)
+#define SIUL2_MSCR_SMC  GENMASK(7, 4)
+#define SIUL2_MSCR_SSS  GENMASK(2, 0)
+
+#define SIUL2_IMCR      0xA40
+#define SIUL2_IMCR_SSS  GENMASK(2, 0)
+
 #else
-#define NXP_S32_SIUL2_IDX(n) (n == 0 ? IP_SIUL2 : NULL)
-#endif
+#error "platform not supported"
+#endif /* CONFIG_SOC_SERIES_S32K3 */
 
-#define NXP_S32_INPUT_BUFFER(group)							\
-	COND_CODE_1(DT_PROP(group, input_enable), (PORT_INPUT_BUFFER_ENABLED),		\
-		(PORT_INPUT_BUFFER_DISABLED))
+#define SIUL2_HAS_ONE_BIT_SLEW_RATE(group)                                                         \
+	UTIL_OR(DT_ENUM_HAS_VALUE(group, slew_rate, slowest),                                      \
+		DT_ENUM_HAS_VALUE(group, slew_rate, fastest))
 
-#define NXP_S32_OUTPUT_BUFFER(group)							\
-	COND_CODE_1(DT_PROP(group, output_enable), (PORT_OUTPUT_BUFFER_ENABLED),	\
-		(PORT_OUTPUT_BUFFER_DISABLED))
-
-#define NXP_S32_INPUT_MUX_REG(group, val)						\
-	COND_CODE_1(DT_PROP(group, input_enable), (NXP_S32_PINMUX_GET_IMCR_IDX(val)),	\
-		(0U))
-
-#define NXP_S32_INPUT_MUX(group, val)							\
-	COND_CODE_1(DT_PROP(group, input_enable),					\
-		((Siul2_Port_Ip_PortInputMux)NXP_S32_PINMUX_GET_IMCR_SSS(val)),		\
-		(PORT_INPUT_MUX_NO_INIT))
-
-#define NXP_S32_PULL_SEL(group)								\
-	COND_CODE_1(DT_PROP(group, bias_pull_up), (PORT_INTERNAL_PULL_UP_ENABLED),	\
-		(COND_CODE_1(DT_PROP(group, bias_pull_down),				\
-		(PORT_INTERNAL_PULL_DOWN_ENABLED), (PORT_INTERNAL_PULL_NOT_ENABLED))))
-
-#if defined(SIUL2_PORT_IP_HAS_ONEBIT_SLEWRATE)
-#define NXP_S32_SLEW_RATE(group)							\
-	COND_CODE_1(DT_NODE_HAS_PROP(group, slew_rate),					\
-		(UTIL_CAT(PORT_SLEW_RATE_, DT_STRING_UPPER_TOKEN(group, slew_rate))),	\
-		(PORT_SLEW_RATE_FASTEST))
-#else
-#define NXP_S32_SLEW_RATE(group)							\
-	COND_CODE_1(DT_NODE_HAS_PROP(group, slew_rate),					\
-		(UTIL_CAT(PORT_SLEW_RATE_CONTROL, DT_PROP(group, slew_rate))),		\
-		(PORT_SLEW_RATE_CONTROL0))
-#endif
-
-#define NXP_S32_DRIVE_STRENGTH(group)							\
-	COND_CODE_1(DT_PROP(group, nxp_drive_strength),					\
-		(PORT_DRIVE_STRENTGTH_ENABLED), (PORT_DRIVE_STRENTGTH_DISABLED))
-
-#define NXP_S32_INVERT(group)								\
-	COND_CODE_1(DT_PROP(group, nxp_invert),						\
-		(PORT_INVERT_ENABLED), (PORT_INVERT_DISABLED))
-
-/* To enable open drain both OBE and ODE bits need to be set */
-#define NXP_S32_OPEN_DRAIN(group)							\
-	(DT_PROP(group, drive_open_drain) && DT_PROP(group, output_enable) ?		\
-		(PORT_OPEN_DRAIN_ENABLED) : (PORT_OPEN_DRAIN_DISABLED))
-
-/* Only a single input mux is configured, the rest is not used */
-#define NXP_S32_INPUT_MUX_NO_INIT							\
-	[1 ... (FEATURE_SIUL2_MAX_NUMBER_OF_INPUT-1)] = PORT_INPUT_MUX_NO_INIT
-
-#define NXP_S32_PINMUX_INIT(group, val)							\
-	.base = NXP_S32_SIUL2_IDX(NXP_S32_PINMUX_GET_SIUL2_IDX(val)),			\
-	.pinPortIdx = NXP_S32_PINMUX_GET_MSCR_IDX(val),					\
-	.mux = (Siul2_Port_Ip_PortMux)NXP_S32_PINMUX_GET_MSCR_SSS(val),			\
-	.inputMux = {									\
-		NXP_S32_INPUT_MUX(group, val),						\
-		NXP_S32_INPUT_MUX_NO_INIT						\
-	},										\
-	.inputMuxReg = {								\
-		NXP_S32_INPUT_MUX_REG(group, val)					\
-	},										\
-	.inputBuffer = NXP_S32_INPUT_BUFFER(group),					\
-	.outputBuffer = NXP_S32_OUTPUT_BUFFER(group),					\
-	.pullConfig = NXP_S32_PULL_SEL(group),						\
-	.safeMode = PORT_SAFE_MODE_DISABLED,						\
-	.slewRateCtrlSel = NXP_S32_SLEW_RATE(group),					\
-	.initValue = PORT_PIN_LEVEL_NOTCHANGED_U8,					\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_DRIVE_STRENGTH,		\
-		(.driveStrength = NXP_S32_DRIVE_STRENGTH(group),))			\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_INVERT_DATA,			\
-		(.invert = NXP_S32_INVERT(group),))					\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_OPEN_DRAIN,			\
-		(.openDrain = NXP_S32_OPEN_DRAIN(group),))				\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_INPUT_FILTER,			\
-		(.inputFilter = PORT_INPUT_FILTER_DISABLED,))				\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_RECEIVER_SELECT,		\
-		(.receiverSel = PORT_RECEIVER_ENABLE_SINGLE_ENDED,))			\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_HYSTERESIS,			\
-		(.hysteresis = PORT_HYSTERESIS_DISABLED,))				\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_ANALOG_PAD_CONTROL,		\
-		(.analogPadControl = PORT_ANALOG_PAD_CONTROL_DISABLED,))		\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_TERMINATION_RESISTOR,		\
-		(.terminationResistor = PORT_TERMINATION_RESISTOR_DISABLED,))		\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_CURRENT_REFERENCE_CONTROL,	\
-		(.currentReferenceControl = PORT_CURRENT_REFERENCE_CONTROL_DISABLED,))	\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_RX_CURRENT_BOOST,		\
-		(.rxCurrentBoost = PORT_RX_CURRENT_BOOST_DISABLED,))			\
-	IF_ENABLED(__DEBRACKET FEATURE_SIUL2_PORT_IP_HAS_PULL_KEEPER,			\
-		(.pullKeep = PORT_PULL_KEEP_DISABLED,))
+#define NXP_S32_PINMUX_INIT(group, val)                                                            \
+	.siul2_idx = NXP_S32_PINMUX_GET_SIUL2_IDX(val),                                            \
+	.imcr_idx = NXP_S32_PINMUX_GET_IMCR_IDX(val),                                              \
+	.imcr_val = FIELD_PREP(SIUL2_IMCR_SSS, NXP_S32_PINMUX_GET_IMCR_SSS(val)),                  \
+	.mscr_idx = NXP_S32_PINMUX_GET_MSCR_IDX(val),                                              \
+	.mscr_val = FIELD_PREP(SIUL2_MSCR_SSS, NXP_S32_PINMUX_GET_MSCR_SSS(val)) |                 \
+		    FIELD_PREP(SIUL2_MSCR_OBE, DT_PROP(group, output_enable)) |                    \
+		    FIELD_PREP(SIUL2_MSCR_IBE, DT_PROP(group, input_enable)) |                     \
+		    FIELD_PREP(SIUL2_MSCR_PUE,                                                     \
+			       DT_PROP(group, bias_pull_up) || DT_PROP(group, bias_pull_down)) |   \
+		    FIELD_PREP(SIUL2_MSCR_PUS, DT_PROP(group, bias_pull_up)) |                     \
+		    UTIL_AND(SIUL2_HAS_ONE_BIT_SLEW_RATE(group),                                   \
+			     FIELD_PREP(SIUL2_MSCR_SRC, DT_ENUM_IDX(group, slew_rate))) |          \
+		    UTIL_AND(UTIL_NOT(SIUL2_HAS_ONE_BIT_SLEW_RATE(group)),                         \
+			     FIELD_PREP(SIUL2_MSCR_SRE, DT_PROP(group, slew_rate))) |              \
+		    UTIL_AND(DT_NODE_HAS_PROP(group, nxp_drive_strength),                          \
+			     (FIELD_PREP(SIUL2_MSCR_DSE, DT_PROP(group, nxp_drive_strength)))) |   \
+		    UTIL_AND(DT_NODE_HAS_PROP(group, nxp_invert),                                  \
+			     (FIELD_PREP(SIUL2_MSCR_INV, DT_PROP(group, nxp_invert)))) |           \
+		    UTIL_AND(DT_NODE_HAS_PROP(group, drive_open_drain),                            \
+			     (FIELD_PREP(SIUL2_MSCR_ODE, (DT_PROP(group, drive_open_drain) &&      \
+							  DT_PROP(group, output_enable)))))
 
 /**
  * @brief Utility macro to initialize each pin.
@@ -132,10 +104,8 @@ typedef Siul2_Port_Ip_PinSettingsConfig pinctrl_soc_pin_t;
  * @param prop Property name.
  * @param idx Property entry index.
  */
-#define Z_PINCTRL_STATE_PIN_INIT(group, prop, idx)					\
-	{										\
-		NXP_S32_PINMUX_INIT(group, DT_PROP_BY_IDX(group, prop, idx))		\
-	},
+#define Z_PINCTRL_STATE_PIN_INIT(group, prop, idx)                                                 \
+	{NXP_S32_PINMUX_INIT(group, DT_PROP_BY_IDX(group, prop, idx))},
 
 /**
  * @brief Utility macro to initialize state pins contained in a given property.
@@ -143,10 +113,11 @@ typedef Siul2_Port_Ip_PinSettingsConfig pinctrl_soc_pin_t;
  * @param node_id Node identifier.
  * @param prop Property name describing state pins.
  */
-#define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)				\
-	{DT_FOREACH_CHILD_VARGS(DT_PHANDLE(node_id, prop),			\
-				DT_FOREACH_PROP_ELEM, pinmux,			\
-				Z_PINCTRL_STATE_PIN_INIT)}
+#define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)                                                   \
+	{                                                                                          \
+		DT_FOREACH_CHILD_VARGS(DT_PHANDLE(node_id, prop), DT_FOREACH_PROP_ELEM, pinmux,    \
+				       Z_PINCTRL_STATE_PIN_INIT)                                   \
+	}
 
 /** @endcond */
 
